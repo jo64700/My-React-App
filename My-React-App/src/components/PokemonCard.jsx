4ce607db064b8648f1b0861974389bbda7ba984a
jo64700 <jonathanbarone64@gmail.com>
2023-03-29 14:57:24 +0200

    OK

--- My-React-App/src/components/PokemonCard.jsx
+++ My-React-App/src/components/PokemonCard.jsx
@@ -1,14 +1,24 @@
-import react from "react";
 
 const PokemonCard = () => {
 
     return (
         <figure className="card">
-            <img src="https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/1.png" alt="bulbasaur" className="card-img" />
-            <figcaption>bulbasaur</figcaption>
+            {pokemonList[0].imgSrc ? <img src={pokemonList[0].imgSrc} alt={pokemonList[0].name} className="card-img" /> : <p>???</p>}
+            <figcaption>{pokemonList[0].name}</figcaption>
         </figure>
     )
 }
+const pokemonList = [
+    {
+        name: "bulbasaur",
+        imgSrc:
+            "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/1.png",
+    },
+    {
+        name: "mew",
+    },
+];
+
 
 
 export default PokemonCard
