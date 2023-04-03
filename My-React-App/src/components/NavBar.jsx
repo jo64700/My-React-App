73c5e71994aaa3df546ef8d3974381a611eee4d4
jo64700 <jonathanbarone64@gmail.com>
2023-04-03 12:17:09 +0200

    Exo 8

--- My-React-App/src/components/NavBar.jsx
+++ My-React-App/src/components/NavBar.jsx
@@ -1,30 +1,21 @@
 import React from 'react';
 
 
-const buttonPokemon = [
-    { name: "Bulbasaur" },
-    { name: "Charmander" },
-    { name: "Squirtle" },
-    { name: "Pikachu" },
-    { name: "Mew" },
-];
-
-function NavBar({ count, setCount, pokemonList }) {
-
-    const handleClickNext = () => {
-        setCount(count + 1)
-    }
-    const handleClickPrevious = () => {
-        setCount(count - 1)
+function NavBar({ index, pokemonList }) {
+    const handlePokemonClick = (index) => {
+        setCount(index);
     }
 
     return (
         <div className='btnContainer'>
-            {count > 0 ? <button className="btnIndexSelector" onClick={handleClickPrevious}>précédant</button> : <button className="btnIndexSelector">dernier</button>}
-            {count < pokemonList.length - 1 ? <button className="btnIndexSelector" onClick={handleClickNext}>suivant</button> : <button className="btnIndexSelector">Fin</button>}
+
+            {pokemonList.map((pokemon, index) => (
+                <button key={pokemon.index} onClick={() => handlePokemonClick(index)}>{pokemon.name}</button>
+            ))}
 
 
-        </div>
+
+        </div >
     )
 }
 
